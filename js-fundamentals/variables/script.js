b67764464f06650a7fce0dbbab4e50b2e146a712
Sashankr <sashankr97@gmail.com>
2025-02-07 00:42:13 +0530

    constant values

--- js-fundamentals/variables/script.js
+++ js-fundamentals/variables/script.js
@@ -1,4 +1,10 @@
 let admin;
-let name = "John";
-admin = name;
+let username = "John";
+admin = username;
 console.log(admin);
+
+let planetName = "Earth";
+let currentVisitor = "Sashank";
+
+const BIRTHDAY = "18.04.1982";
+const age = someCode(BIRTHDAY);
