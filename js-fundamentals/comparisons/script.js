835512e67d656281d74798ee00347d1bfab045fa
Sashankr <sashankr97@gmail.com>
2025-02-07 20:20:43 +0530

    strict equals and equals

---
+++ js-fundamentals/comparisons/script.js
@@ -0,0 +1,7 @@
+5 > 4; // true
+"apple" > "pineapple"; // false
+"2" > "12"; // true character 2 compared to character 1
+undefined == null; //true
+undefined === null; // false
+null == "\n0\n"; // false
+null === +"\n0\n"; // false
