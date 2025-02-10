13577979f155a3fb242c2de6321d1e4da4e150b3
Sashankr <sashankr97@gmail.com>
2025-02-11 01:05:54 +0530

    conditional

---
+++ js-fundamentals/conditional-branching/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const res = prompt(`What's the "official" name of Javascript?`);
+if (res === "ECMAScript" || res === "ecmascript") {
+  alert("Right!");
+} else {
+  alert(`You don't know "ECMAScript"!`);
+}
