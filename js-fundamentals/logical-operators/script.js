becb87e03f335bffe6d45ad9ef8b8a541bca8591
Sashankr <sashankr97@gmail.com>
2025-02-12 00:39:15 +0530

    NOT operator and double excalimation

--- js-fundamentals/logical-operators/script.js
+++ js-fundamentals/logical-operators/script.js
@@ -3,3 +3,7 @@ console.log(1 || false); // or returns the first truthy expression and ignores t
 console.log(0 || undefined || null); // if it doesn't find any truty values it returns last value
 
 console.log(1 && null && undefined); // AND on the other hand returns the first falsey value and ignores the rest.
+
+console.log(!false); // NOT operator converts to boolean and reverses the value
+console.log(!!null); // this is shorthand way of converting falsey values to boolean.
+console.log(Boolean(null));
