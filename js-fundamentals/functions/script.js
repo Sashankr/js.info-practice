4c56db815b6562a59bc6560467f2b8b6c1a9ee9d
Sashankr <sashankr97@gmail.com>
2025-02-20 01:04:52 +0530

    function basic practice

--- js-fundamentals/functions/script.js
+++ js-fundamentals/functions/script.js
@@ -51,16 +51,46 @@ Make two variants of checkAge:
 */
 // with ? operator
 
-function checkAge3(age) {
-  age > 18 ? true : confirm("Did parents allow you?");
-}
+// function checkAge3(age) {
+//   age > 18 ? true : confirm("Did parents allow you?");
+// }
 
-checkAge3(15);
+// checkAge3(15);
 
 // with OR ||
 
-function checkAge3(age) {
-  age > 18 || confirm("Did parents allow you?");
+// function checkAge3(age) {
+//   age > 18 || confirm("Did parents allow you?");
+// }
+
+// checkAge3(15);
+
+// q3. Write a function min(a,b) which returns the least of two numbers a and b.
+
+/* Test Cases
+min(2, 5) == 2
+min(3, -1) == -1
+min(1, 1) == 1
+*/
+
+function min(a, b) {
+  return a < b ? a : b;
 }
 
-checkAge3(15);
+console.log({
+  1: min(2, 5),
+  2: min(3, -1),
+  3: min(1, 1),
+});
+
+// q4. Function pow(x,n)
+
+// Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
+
+function pow(x, n) {
+  if (n < 1) {
+    throw Error("Not a natural number");
+  }
+}
+
+console.log(pow(10, 2));
