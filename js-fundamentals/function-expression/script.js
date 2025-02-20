4ca0f22668d345b2a51c7a9b65235eea54f13c60
Sashankr <sashankr97@gmail.com>
2025-02-20 12:57:19 +0530

    update function expressions with arrow functions

--- js-fundamentals/function-expression/script.js
+++ js-fundamentals/function-expression/script.js
@@ -23,3 +23,31 @@ function displayCancel() {
 }
 
 answer("are you sure?", displayOk, displayCancel);
+
+// convert the following function expressions with arrow functions
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+
+// ask(
+//   "Do you agree?",
+//   function () {
+//     alert("You agreed.");
+//   },
+//   function () {
+//     alert("You canceled the execution.");
+//   }
+// );
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+);
