56498191a146352c8d071221ea080dccb4af5b07
Sashankr <sashankr97@gmail.com>
2025-03-07 13:58:59 +0530

    computed property object

--- js-fundamentals/objects/intro/script.js
+++ js-fundamentals/objects/intro/script.js
@@ -15,3 +15,48 @@ console.log(user);
 // runtime expression to get value of a key from an object.
 let key = "last name";
 console.log(user[key]);
+
+// computed property
+// We can accept a value from user and then create that key on runtime while creating an object.
+
+const fruit = prompt("Enter fruit you want to buy");
+const bag = {
+  [fruit]: 5,
+};
+
+console.log("my bag", bag);
+
+// Object property shorthand
+
+function createName(firstName, lastName) {
+  return {
+    firstName: firstName,
+    lastName: lastName,
+  };
+}
+console.log(createName("S", "R"));
+
+function createName2(firstName, lastName) {
+  return {
+    firstName,
+    lastName,
+  };
+}
+
+console.log(createName2("S", "R"));
+
+const newObj = {
+  for: "hello",
+  let: "bye",
+  return: "return",
+  0: "zero num", // auto converts to string
+  0: "zero string",
+};
+
+console.log(newObj.for, newObj.let, newObj.return);
+
+console.log(newObj[0], newObj["0"]); // both work
+
+const obj3 = {};
+obj3.__proto__ = 5;
+console.log(obj3.__proto__);
