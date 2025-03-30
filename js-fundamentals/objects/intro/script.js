31676a14a6ee76f8e40dec0230b247d7adcb4234
Sashankr <sashankr97@gmail.com>
2025-03-30 19:31:57 +0530

    ex1: objects

--- js-fundamentals/objects/intro/script.js
+++ js-fundamentals/objects/intro/script.js
@@ -1,62 +1,17 @@
-// creating a new object
-const user = new Object(); // Object constructor.
-const user1 = {}; // object literal
+// tasks
 
-console.log(user, user1);
+// 1. Write the code, one line for each action:
 
-// sqaure bracket notation
+// Create an empty object user.
+// Add the property name with the value John.
+// Add the property surname with the value Smith.
+// Change the value of the name to Pete.
+// Remove the property name from the object.
 
-user["first name"] = "Sashank";
-user["last name"] = "R";
-console.log(user["first name"], user);
-delete user["first name"];
-console.log(user);
-
-// runtime expression to get value of a key from an object.
-let key = "last name";
-console.log(user[key]);
-
-// computed property
-// We can accept a value from user and then create that key on runtime while creating an object.
-
-const fruit = prompt("Enter fruit you want to buy");
-const bag = {
-  [fruit]: 5,
+const user = {
+  name: "John",
+  surname: "Smith",
 };
 
-console.log("my bag", bag);
-
-// Object property shorthand
-
-function createName(firstName, lastName) {
-  return {
-    firstName: firstName,
-    lastName: lastName,
-  };
-}
-console.log(createName("S", "R"));
-
-function createName2(firstName, lastName) {
-  return {
-    firstName,
-    lastName,
-  };
-}
-
-console.log(createName2("S", "R"));
-
-const newObj = {
-  for: "hello",
-  let: "bye",
-  return: "return",
-  0: "zero num", // auto converts to string
-  0: "zero string",
-};
-
-console.log(newObj.for, newObj.let, newObj.return);
-
-console.log(newObj[0], newObj["0"]); // both work
-
-const obj3 = {};
-obj3.__proto__ = 5;
-console.log(obj3.__proto__);
+user.name = "Pete";
+delete user.name;
