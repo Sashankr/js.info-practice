41eb624577efb266e3419320e159a739c776d4c3
Sashankr <sashankr97@gmail.com>
2025-03-30 20:15:56 +0530

    structured clone

--- js-fundamentals/objects/cloning-compare/script.js
+++ js-fundamentals/objects/cloning-compare/script.js
@@ -13,6 +13,21 @@ newCopy.name = "Joe";
 
 console.log(newCopy, person);
 
-// const person2 = {
-//     name : 'John'
-// }
+const person2 = {
+  name: "John",
+  role: "Dev",
+  location: {
+    state: "NY",
+    country: "USA",
+  },
+};
+
+const clone2 = Object.assign(person2);
+clone2.location.state = "TX";
+console.log(clone2, person2); // the state in person 2 also gets updated
+
+// to prevent state in person 2 to get updated use structuredClone
+
+const cloneStructured = structuredClone(person2);
+cloneStructured.location.state = "AZ";
+console.log(cloneStructured, person2); // state not updated this time
