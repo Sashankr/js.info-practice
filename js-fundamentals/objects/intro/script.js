f090d446ae6837d052f710d203af5feeb1dc826b
Sashankr <sashankr97@gmail.com>
2025-03-30 19:39:43 +0530

    ex4: objects

--- js-fundamentals/objects/intro/script.js
+++ js-fundamentals/objects/intro/script.js
@@ -58,3 +58,39 @@ for (let key in salaries) {
   sum += salaries[key];
 }
 console.log("salaries sum", sum);
+
+// 4. Multiply numeric property values by 2
+// Create a function multiplyNumeric(obj) that multiplies all numeric property values of obj by 2.
+
+// before the call
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu"
+// };
+
+// multiplyNumeric(menu);
+
+// // after the call
+// menu = {
+//   width: 400,
+//   height: 600,
+//   title: "My menu"
+// };
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
+};
+
+function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === "number") {
+      obj[key] *= 2;
+    }
+  }
+}
+
+multiplyNumeric(menu);
+console.log("menu", menu);
