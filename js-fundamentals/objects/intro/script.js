ea5800dbd1aa0a94c74746a005a55608e8707d21
Sashankr <sashankr97@gmail.com>
2025-03-30 19:34:34 +0530

    ex2: objects

--- js-fundamentals/objects/intro/script.js
+++ js-fundamentals/objects/intro/script.js
@@ -15,3 +15,30 @@ const user = {
 
 user.name = "Pete";
 delete user.name;
+
+// 2. Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+// Should work like that:
+
+// let schedule = {};
+
+// alert( isEmpty(schedule) ); // true
+
+// schedule["8:30"] = "get up";
+
+// alert( isEmpty(schedule) ); // false
+
+function isEmpty(obj) {
+  if (Object.keys(obj).length === 0) {
+    return true;
+  }
+  return false;
+}
+
+let schedule = {};
+
+alert(isEmpty(schedule)); // true
+
+schedule["8:30"] = "get up";
+
+alert(isEmpty(schedule)); // false
