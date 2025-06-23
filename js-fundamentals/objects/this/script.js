ab7df737475e484336a8d10fd85ceba95069a5e0
SashankRampalli <sashank@mindwavesolutions.com>
2025-06-23 09:35:17 +0530

    chaining feature in object methods

--- js-fundamentals/objects/this/script.js
+++ js-fundamentals/objects/this/script.js
@@ -40,6 +40,55 @@ const calculator = {
   },
 };
 
-calculator.read();
-calculator.sum();
-calculator.multiply();
+// calculator.read();
+// calculator.sum();
+// calculator.multiply();
+
+// Q3. Chaining
+
+// There’s a ladder object that allows you to go up and down:
+
+// let ladder = {
+//   step: 0,
+//   up() {
+//     this.step++;
+//   },
+//   down() {
+//     this.step--;
+//   },
+//   showStep: function() { // shows the current step
+//     alert( this.step );
+//   }
+// };
+
+// Now, if we need to make several calls in sequence, we can do it like this:
+
+/*
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+ladder.down();
+ladder.showStep(); // 0
+*/
+
+// To allow chaining we can return this from each method.
+
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
+  },
+  down() {
+    this.step--;
+    return this;
+  },
+  showStep: function () {
+    // shows the current step
+    alert(this.step);
+    return this;
+  },
+};
+
+ladder.up().up().down().showStep().down().showStep(); // shows 1 then 0
