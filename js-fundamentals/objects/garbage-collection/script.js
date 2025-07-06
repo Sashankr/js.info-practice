bcd8933fc02d83e1b1d8803dd4797951bc5e427f
SashankR <sashankr97@gmail.com>
2025-07-06 07:36:16 +0530

    Sashankr:garbage collection-nested object references

--- js-fundamentals/objects/garbage-collection/script.js
+++ js-fundamentals/objects/garbage-collection/script.js
@@ -6,3 +6,19 @@ const admin = user;
 user = null;
 console.log(admin.name);
 // Since admin is still reachable it's not garbage collected
+
+function marry(man, women) {
+  man.wife = women;
+  women.husband = man;
+  return {
+    father: man,
+    mother: women,
+  };
+}
+
+const couple = marry({ name: "John" }, { name: "Ann" });
+console.log(couple);
+delete couple.father;
+delete couple.mother.husband;
+
+console.log(couple);
