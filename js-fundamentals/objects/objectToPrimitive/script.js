d491c58cd0b28601ec0878f66eaf07dfef183bb4
SashankR <sashankr97@gmail.com>
2025-07-09 11:29:47 +0530

    toString and valueOf methods

--- js-fundamentals/objects/objectToPrimitive/script.js
+++ js-fundamentals/objects/objectToPrimitive/script.js
@@ -2,11 +2,31 @@ const user = {
   name: "john",
   savings: 1000,
   [Symbol.toPrimitive](hint) {
-    alert(hint);
+    console.log(hint);
     return hint === "string" ? `{user : ${this.name}}` : this.savings;
   },
 };
 
-alert(user); //string `{user : "john"}`
-alert(+user); //number // 1000
-alert(user + 100); // default // 1500
+console.log(user); //string `{user : "john"}`
+console.log(+user); //number // 1000
+console.log(user + 100); // default // 1500
+
+// Now lets see an example where Symbol.toPrimitive is not available.
+
+const dev = {
+  name: "Sashank",
+  exp: 4,
+  // hint is string
+  toString() {
+    return `{name: "${this.name}"}`;
+  },
+  // hint number
+  valueOf() {
+    return this.exp;
+  },
+};
+console.log("to string, value of");
+
+alert(dev); // toString
+console.log("2", +dev); // valuOf 4
+console.log("3", dev + 1); // valueOf 5
