a37b58970f1e1617b6dbb35af431d4a1b95bc395
Sashankr <sashankr97@gmail.com>
2025-07-10 23:49:28 +0530

    error in strict mode

--- js-fundamentals/data-type-chapter/primitive-methods/script.js
+++ js-fundamentals/data-type-chapter/primitive-methods/script.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 In Javascript, Primitives have methods like objects but how do primitives have methods they are not objects right?
 We learned that primitives are only used to store a single value, then how are these methods accessible on them?
@@ -23,4 +24,11 @@ console.log(str.startsWith("h"));
 const str2 = "Bye";
 str2.value = 10;
 
-console.log(str2.value);
+console.log(str2.value); // undefined in non strict mode, error in strict mode.
+
+/* So what happens in the above example is
+line 1 : the string is stored in variable, the wrapper object String is created.
+line 2 : the value property with value 10 is added to String object. 
+line 3 : but here since the wrapper object is deleted immediately, the value property reference is also lost.
+
+*/
