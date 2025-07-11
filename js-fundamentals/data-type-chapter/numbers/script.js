2eaa50eccd1f2f1c5f424827bb0a025535c7bc78
Sashankr <sashankr97@gmail.com>
2025-07-11 23:48:47 +0530

    representing numbers

---
+++ js-fundamentals/data-type-chapter/numbers/script.js
@@ -0,0 +1,11 @@
+// const oneBillion = 1000000000;
+// const oneBillion = 1_000_000_000; // to make it more readable we can use underscore to separate them.
+const oneBillion = 1e9;
+console.log(oneBillion);
+const sevenBillion300million = 7.3e9;
+console.log(sevenBillion300million);
+
+let mcs = 0.000001; // 1 microsecond is 1 millionth of a second.
+// we can represent by e- notation
+mcs = 1e-6;
+console.log(mcs);
