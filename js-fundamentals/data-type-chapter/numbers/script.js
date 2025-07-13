6ada721f4a8c48e5830a6efc1bbb941f8d0163cb
Sashankr <sashankr97@gmail.com>
2025-07-13 15:04:32 +0530

    direct toString method used on number

--- js-fundamentals/data-type-chapter/numbers/script.js
+++ js-fundamentals/data-type-chapter/numbers/script.js
@@ -13,3 +13,14 @@ console.log(mcs);
 const num1 = `0xFF`; // 255 in hexadecinal.
 const num2 = `0b11111111`; // 255 in binary.
 const num3 = `0o377`; // 255 in octal.
+
+// toString method: converts number to string with optional base value
+
+const n4 = 124415;
+n4.toString(16); // hexa
+n4.toString(2); // binary
+n4.toString(36); // max character support
+
+// directly using toString on number
+
+console.log((1234).toString(16));
