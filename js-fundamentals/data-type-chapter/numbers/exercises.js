ec78f895a6e57f5dc074c096e3af1e391aee1002
Sashankr <sashankr97@gmail.com>
2025-07-14 11:42:39 +0530

    convert user input from prompt and add

---
+++ js-fundamentals/data-type-chapter/numbers/exercises.js
@@ -0,0 +1,7 @@
+// 1. Sum Numbers from visitor
+// Create a script that prompts the visitor to enter two numbers and then shows their sum.
+// P.S. There is a gotcha with types.
+
+const input1 = prompt("Enter first number");
+const input2 = prompt("Enter second number");
+alert(`Sum : ${+input1} + ${+input2} = ${+input1 + +input2}`);
