e1bf8695bc52cd1b6456ef94ca58c2bc67c75a6d
Sashankr <sashankr97@gmail.com>
2025-07-15 12:45:38 +0530

    type of strings

---
+++ js-fundamentals/data-type-chapter/strings/script.js
@@ -0,0 +1,14 @@
+// Strings : Represent texual data in UTF-16 format.
+
+// Types of quotes
+
+console.log("Hello"); // double
+console.log("Hello"); // single
+const name = "Sashank";
+console.log(`Hello ${name}`);
+console.log(`Guests list:
+    1. John
+    2. Jacob
+    3. Sam
+    4. Sarah
+    `);
