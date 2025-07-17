ac62e46b3cecf01fa04f2bb6baf7cc3dc39b5d0a
Sashankr <sashankr97@gmail.com>
2025-07-17 07:53:10 +0530

    basic array features

--- js-fundamentals/data-type-chapter/arrays/script.js
+++ js-fundamentals/data-type-chapter/arrays/script.js
@@ -1,3 +1,17 @@
 // ways to create an array
 const arr = [];
 const arr1 = new Array();
+
+const fruits = ["Apple", "Mango", "Banana"];
+
+// accessing elements of array based on index
+console.log(fruits[0]);
+
+// updating element of array based on index
+fruits[1] = "Pineapple";
+console.log(fruits);
+
+// adding new element to array
+
+fruits[3] = "Grapes";
+console.log(fruits);
