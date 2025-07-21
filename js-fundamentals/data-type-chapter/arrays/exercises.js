ba201b3d1b0764effb0badc83ade285ac4592df7
Sashankr <sashankr97@gmail.com>
2025-07-21 12:36:59 +0530

    array methods exercise

--- js-fundamentals/data-type-chapter/arrays/exercises.js
+++ js-fundamentals/data-type-chapter/arrays/exercises.js
@@ -8,3 +8,42 @@ shoppingCart.push("Banana");
 
 // what's in fruits?
 console.log(fruits.length); // 4
+
+// Exercise 2
+
+/*
+Let’s try 5 array operations.
+
+Create an array styles with items “Jazz” and “Blues”.
+
+Append “Rock-n-Roll” to the end.
+Replace the value in the middle with “Classics”. 
+Your code for finding the middle value should work for any arrays with odd length.
+Strip off the first value of the array and show it.
+Prepend Rap and Reggae to the array.
+The array in the process:
+Jazz, Blues
+Jazz, Blues, Rock-n-Roll
+Jazz, Classics, Rock-n-Roll
+Classics, Rock-n-Roll
+Rap, Reggae, Classics, Rock-n-Roll
+*/
+
+function removeMiddleElement(arr) {
+  const middleIndex = Math.floor(arr.length / 2);
+  arr.splice(middleIndex, 1, "Classics");
+
+  return arr;
+}
+
+let music = ["Jazz", "Blues"];
+console.log("music", music);
+music.push("Rock-n-Roll");
+console.log("music", music);
+music = removeMiddleElement(music);
+console.log("music", music);
+music.shift();
+console.log("music", music);
+
+music.unshift("Rap", "Reggae");
+console.log("music", music);
