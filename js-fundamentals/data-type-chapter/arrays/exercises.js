932c1803191edcb0ce8660897b97ad4f7a076d63
Sashankr <sashankr97@gmail.com>
2025-07-22 08:22:41 +0530

    maxSubArray Sum

--- js-fundamentals/data-type-chapter/arrays/exercises.js
+++ js-fundamentals/data-type-chapter/arrays/exercises.js
@@ -71,7 +71,7 @@ arr[2](); // ['a','b',[Function (anonymous)]]
 
 const userInputs = [];
 while (true) {
-  let input = prompt("Enter a number to add");
+  let input = prompt("Enter a number to add") || "";
 
   if (input === null || input === "" || !isFinite(input)) {
     break;
@@ -84,3 +84,49 @@ for (let num of userInputs) {
   sum += num;
 }
 console.log(`sum: ${sum}`);
+
+// Exercise 5
+
+/*
+The input is an array of numbers, e.g. arr = [1, -2, 3, 4, -9, 6].
+
+The task is: find the contiguous subarray of arr with the maximal sum of items.
+
+Write the function getMaxSubSum(arr) that will return that sum.
+
+For instance:
+getMaxSubSum([-1, 2, 3, -9]) == 5 (the sum of highlighted items)
+getMaxSubSum([2, -1, 2, 3, -9]) == 6
+getMaxSubSum([-1, 2, 3, -9, 11]) == 11
+getMaxSubSum([-2, -1, 1, 2]) == 3
+getMaxSubSum([100, -9, 2, -3, 5]) == 100
+getMaxSubSum([1, 2, 3]) == 6 (take all)
+
+If all items are negative, it means that we take none (the subarray is empty), so the sum is zero:
+
+getMaxSubSum([-1, -2, -3]) = 0
+
+Please try to think of a fast solution: O(n2) or even O(n) if you can.
+
+*/
+
+function getMaxSubSum(arr) {
+  let maxSum = 0;
+  let partialSum = 0;
+
+  for (let item of arr) {
+    // for each item of arr
+    partialSum += item; // add it to partialSum
+    maxSum = Math.max(maxSum, partialSum); // remember the maximum
+    if (partialSum < 0) partialSum = 0; // zero if negative
+  }
+
+  return maxSum;
+}
+
+console.log(getMaxSubSum([-1, 2, 3, -9])); // 5
+console.log(getMaxSubSum([-1, 2, 3, -9, 11])); // 11
+console.log(getMaxSubSum([-2, -1, 1, 2])); // 3
+console.log(getMaxSubSum([100, -9, 2, -3, 5])); // 100
+console.log(getMaxSubSum([1, 2, 3])); // 6
+console.log(getMaxSubSum([-1, -2, -3])); // 0
