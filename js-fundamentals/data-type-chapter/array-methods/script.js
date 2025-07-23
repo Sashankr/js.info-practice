3105c0f7c68a1d71261440aa0736ffe3849d0747
Sashankr <sashankr97@gmail.com>
2025-07-23 23:22:23 +0530

    splice with delete count

---
+++ js-fundamentals/data-type-chapter/array-methods/script.js
@@ -0,0 +1,8 @@
+// Array splice method:
+// It can remove, add, replace items in an array.
+const arr = ["I", "learn", "javascript"];
+console.log("arr", arr);
+// Syntax:
+// arr.splice(start,deleteCount,el1,el2,...eln)
+arr.splice(1, 2);
+console.log("arr", arr); // ['I']
